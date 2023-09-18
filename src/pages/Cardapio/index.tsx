b7dc871fe0aef09149e63cf9aa9898ef18701472
Lucio_Beckler <luciobecklerp@gmail.com>
2023-09-18 19:11:09 -0300

    Adicionando um header rederizado em ambas as rotas

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,31 +4,20 @@ import { useState } from 'react';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
-import Menu from 'components/Menu';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState('');
     const [filtro, setFiltro] = useState<number | null>(null);
     const [ordenador, setOrdenador] = useState('');
     return (
-        <main>
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                    A casa do código e da massa
-                </div>
-            </header>
-            <section className={styles.cardapio}>
-                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                <Buscador busca={busca} setBusca={setBusca} />
-                <div className={styles.cardapio__filtros}>
-                    <Filtros filtro={filtro} setFiltro={setFiltro} />
-                    <Ordenador
-                        ordenador={ordenador}
-                        setOrdenador={setOrdenador}
-                    />
-                </div>
-                <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-            </section>
-        </main>
+        <section className={styles.cardapio}>
+            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+            <Buscador busca={busca} setBusca={setBusca} />
+            <div className={styles.cardapio__filtros}>
+                <Filtros filtro={filtro} setFiltro={setFiltro} />
+                <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+            </div>
+            <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+        </section>
     );
 }
