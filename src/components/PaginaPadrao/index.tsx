0dec3f079c382345017ddc6b95fec6dde84a7de3
Lucio_Beckler <luciobecklerp@gmail.com>
2023-09-21 10:31:50 -0300

    Estilos melhorados

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import styles from './PaginaPadrao.module.scss';
-import { Outlet } from 'react-router-dom';
+import styles from './PaginaPadrao.module.scss'; // Importa os estilos CSS do arquivo 'PaginaPadrao.module.scss'
+import { Outlet } from 'react-router-dom'; // Importa o componente 'Outlet' do pacote 'react-router-dom'
+import stylesTema from '../../styles/Tema.module.scss';
 
 export default function PaginaPadrao() {
+    // Declaração do componente funcional 'PaginaPadrao'
     return (
         <>
             <header className={styles.header}>
+                {' '}
+                {/* Início do cabeçalho */}
                 <div className={styles.header__text}>
-                    A casa do código e da massa
+                    A casa do código e da massa{' '}
+                    {/* Texto exibido no cabeçalho */}
                 </div>
             </header>
-            <div>
-                <Outlet></Outlet>
+            <div className={stylesTema.container}>
+                <Outlet></Outlet>{' '}
+                {/* Renderiza o componente filho definido na rota */}
             </div>
         </>
     );
